320ba9492be4e70784c24e638b67afff22303214
Oleh Krysyuk <olegkrysyuk@gmail.com>
2023-04-26 14:53:02 +0300

    bot2

--- Main.java
+++ Main.java
@@ -2,13 +2,24 @@ package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class Main extends TelegramLongPollingBot {
-//    jvbnbunvbydbvb7567hgf_Bot
+    //    jvbnbunvbydbvb7567hgf_Bot
 //    6124034037:AAHu1-koE-BEiHnGySlsesYQnnKOKWlrlho
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
@@ -31,14 +42,45 @@ public class Main extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         Long chatId = getChatId(update);
 
-        SendMessage msg = createMessege("*Hello*,  "
-                + update.getMessage().getFrom().getFirstName()
-                + update.getMessage().getFrom().getLastName());
-        msg.setChatId(chatId);
-        sendApiMethodAsync(msg);
+        if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
+            SendMessage message = createMessage("Привіт"
+                    + update.getMessage().getFrom().getFirstName()
+                    + update.getMessage().getFrom().getLastName());
+            message.setChatId(chatId);
+            attachButtons(message, Map.of(
+                    "Слава Україні", "glory_to_ukraine"
+            ));
+            sendApiMethodAsync(message);
+        }
+
+        if (update.hasCallbackQuery()) {
+            if (update.getCallbackQuery().getData().equals("glory_to_ukraine")) {
+                SendMessage message = createMessage("Героям Слава!");
+                message.setChatId(chatId);
+                attachButtons(message, Map.of(
+                        "Слава нації", "glory_to_nation"
+                ));
+                sendApiMethodAsync(message);
+            }
+        }
+
+        if (update.hasCallbackQuery()) {
+            if (update.getCallbackQuery().getData().equals("glory_to_nation")) {
+                SendMessage message = createMessage("Смерть ворогам!");
+                message.setChatId(chatId);
+                sendApiMethodAsync(message);
+            }
+        }
+        SendAnimation animation = new SendAnimation();
+        InputFile inputFile = new InputFile();
+        inputFile.setMedia(new File());
+        animation.setAnimation(inputFile);
+        animation.setChatId(chatId);
+        executeAsync(animation);
     }
-    public Long getChatId(Update update){
-        if (update.hasMessage()){
+
+    public Long getChatId(Update update) {
+        if (update.hasMessage()) {
             return update.getMessage().getFrom().getId();
         }
         if (update.hasCallbackQuery()) {
@@ -46,10 +88,31 @@ public class Main extends TelegramLongPollingBot {
         }
         return null;
     }
-    public SendMessage createMessege(String text){
+
+    public SendMessage createMessage(String text) {
         SendMessage message = new SendMessage();
-        message.setText(text);
+        message.setText(new String(text.getBytes(), StandardCharsets.UTF_8));
         message.setParseMode("markdown");
         return message;
+    }
+    //Animation________________________________________________
+
+
+    //______________________________
+    public void attachButtons(SendMessage message, Map<String, String> buttons) {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        for (String buttonName : buttons.keySet()) {
+            String buttonValue = buttons.get(buttonName);
+
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(new String(buttonName.getBytes(), StandardCharsets.UTF_8));
+            button.setCallbackData(buttonValue);
+            keyboard.add(Arrays.asList(button));
+        }
+        markup.setKeyboard(keyboard);
+        message.setReplyMarkup(markup);
     }
 }
